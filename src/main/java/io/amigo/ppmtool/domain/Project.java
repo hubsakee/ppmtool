9621724f82e466ba1788340dd020eb78251a2f6d
hubsakee <hubsakee@protonmail.com>
2020-11-25 13:00:09 +0100

    restoring

--- src/main/java/io/amigo/ppmtool/domain/Project.java
+++ src/main/java/io/amigo/ppmtool/domain/Project.java
@@ -1,9 +1,15 @@
 package io.amigo.ppmtool.domain;
 
-/**
- * Project
- */
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Project {
 
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
